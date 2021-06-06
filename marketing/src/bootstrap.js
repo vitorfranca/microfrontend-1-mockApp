786a78cc670dabbee923f877be2e0db03e0a6f07
Vitor França <vitufr@gmail.com>
2021-06-06 17:16:23 -0300

    setup a marketing and container app running separately

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const mount = el => {
+  ReactDOM.render(
+    <App/>,
+    el,
+  )
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const el = document.getElementById('marketing-root')
+  if (el) {
+    mount(el)
+  }
+}
+
+export { mount }
