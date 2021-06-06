06032fe7b5a117ede75cb9918844c5a54c012010
Vitor França <vitufr@gmail.com>
2021-06-06 17:52:22 -0300

    rendering marketing app inside container

--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import MarketingApp from './components/MarketingApp'
 
 const App = () => (
-  <h1>Hi There</h1>
+  <div>
+    <h1>This is the container app</h1>
+    <hr />
+    <p>Below this is the marketing app</p>
+    <MarketingApp/>
+  </div>
 )
 
 export default App
